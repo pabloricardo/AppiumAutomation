aded13bfe23fc1b5367dc16d7aff251e5e9066ad
Pablo <pablo.mg@hotmail.com>
2020-05-14 22:57:00 -0300

    add first test appium

--- src/Base.java
+++ src/Base.java
@@ -1,15 +1,18 @@
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Driver;
+import java.util.concurrent.TimeUnit;
 
+import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class base {
+public class Base {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
 		
 		File f = new File("src/ApiDemos-debug.apk");
 	
@@ -18,7 +21,8 @@ public class base {
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
 		cap.setCapability(MobileCapabilityType.APP, f.getAbsolutePath());
 		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
 		
 
 	}
---
+++ src/Basic.java
@@ -0,0 +1,12 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+
+public class Basic extends Base {
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Capabilities();
+
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+    }
+}
