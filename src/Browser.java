8cc9d4c237217de6481be6b7fb84a3e2b5d1f2a4
Pablo <pablo.mg@hotmail.com>
2020-05-16 21:11:16 -0300

    add java script executor

--- src/Browser.java
+++ src/Browser.java
@@ -1,5 +1,7 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.net.MalformedURLException;
 
@@ -7,5 +9,7 @@ public class Browser extends BaseChrome {
     public static void main(String[] args) throws MalformedURLException {
         AndroidDriver<AndroidElement> driver = Capabilities();
         driver.get("http://google.com");
+        //JavascriptExecutor jse = (JavascriptExecutor) driver;
+
     }
 }
